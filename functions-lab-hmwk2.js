7073e70bd2834491173b09805bc67f0d0176ccae
Paris Jones <bayarette@gmail.com>
2015-01-14 18:31:41 -0800

    adding answers

---
+++ functions-lab-hmwk2.js
@@ -0,0 +1,38 @@
+/*===============================================
+Problem One
+
+================================================*/
+
+function maxOfTwoNumbers(a, b){
+    if(a > b) {
+        console.log(a);
+    } else console.log(b);
+}
+
+maxOfTwoNumbers(8, 5)
+
+
+/*===============================================
+Problem Two
+
+================================================*/
+
+function maxOfThree(a, b, c) {
+    if(a>b && a>c) {
+        console.log(a);
+    } else if(b>a && b>c) {
+        console.log(b);
+    } else {
+        console.log(c);
+    }
+}
+
+
+maxOfThree(9, 11, 22)
+
+/*===============================================
+Problem Three
+
+================================================*/
+
+
